02debf2eff1f0ebcd8be87e58aee202ff17172a5
Zainab Jaradat <122690418+zainabja52@users.noreply.github.com>
2025-04-11 03:22:09 +0200

    Create RotateArray.cs

---
+++ Leetcode-Problems/RotateArray/RotateArray.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        k %= nums.Length; // To handle k > length
+        Reverse(nums, 0, nums.Length - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, nums.Length - 1);
+    }
+
+    private void Reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
